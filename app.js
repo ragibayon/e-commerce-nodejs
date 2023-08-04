ff52b1c8562d6b4fe9a65284cbf70960f257dc8e
ragibayon <ragib.ayon@live.com>
2023-08-04 18:00:08 +0600

    added pagination features

--- app.js
+++ app.js
@@ -106,12 +106,12 @@ app.use(authRoutes);
 app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
-app.use((error, req, res, next) => {
-  res.status(error.httpStatusCode || 500).render('500', {
-    pageTitle: 'Error',
-    path: '/500',
-  });
-});
+// app.use((error, req, res, next) => {
+//   res.status(error.httpStatusCode || 500).render('500', {
+//     pageTitle: 'Error',
+//     path: '/500',
+//   });
+// });
 
 const connectionString = MONGODB_URI;
 const options = {
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,21 +2,33 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 const PDFDocument = require('pdfkit');
-
 const Product = require('../models/product');
 const Order = require('../models/orders');
 const User = require('../models/user');
 const throwError = require('../util/throwError');
 const {generateInvoice} = require('../util/createInvoice');
 
+const {ITEMS_PER_PAGE} = require('../constants');
+
 exports.getProducts = async (req, res, next) => {
   try {
-    const products = await Product.find();
+    let page = +req.query.page || 1;
+    const totalProducts = await Product.find().countDocuments();
+    const pageCount = Math.ceil(totalProducts / ITEMS_PER_PAGE);
+    if (page > pageCount) {
+      page = 1;
+    }
+
+    const products = await Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
 
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
       path: '/products',
+      pageCount: pageCount,
+      currentPage: page,
     });
   } catch (err) {
     const error = throwError(err, 500);
@@ -42,12 +54,25 @@ exports.getProduct = async (req, res, next) => {
 
 exports.getIndex = async (req, res, next) => {
   try {
-    const products = await Product.find();
+    let page = +req.query.page || 1;
+
+    const totalProducts = await Product.find().countDocuments();
+    const pageCount = Math.ceil(totalProducts / ITEMS_PER_PAGE);
+
+    if (page > pageCount) {
+      page = 1;
+    }
+
+    const products = await Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
 
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
+      pageCount: pageCount,
+      currentPage: page,
     });
   } catch (err) {
     const error = throwError(err, 500);
